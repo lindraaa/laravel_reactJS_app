18fa31575095529a2fbb948cf84b6c843a4c0fa0
unknown <chancruz739@gmail.com>
2024-11-24 08:47:47 +0800

    added navbar and use props children

--- frontend/src/components/Shared/Layout.js
+++ frontend/src/components/Shared/Layout.js
@@ -1,6 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+function Layout(props){
+    return<>
+    <Navbar bg="dark" expand="lg" >
+      <Container>
+        <Navbar.Brand href="#" className='text-light'>Accounts</Navbar.Brand>
+      </Container>
+    </Navbar>
+    <Container>{props.children}</Container>
 
-function Layout(){
-    return<></>
+    </>
 }
 
 export default Layout;
