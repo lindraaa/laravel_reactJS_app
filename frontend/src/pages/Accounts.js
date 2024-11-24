afb38e523400ef1779428219a9f44a2f6f719354
unknown <chancruz739@gmail.com>
2024-11-24 09:43:11 +0800

    calling api in ussEffect

--- frontend/src/pages/Accounts.js
+++ frontend/src/pages/Accounts.js
@@ -1,5 +1,22 @@
-function Accounts(){
-    return<>
+import axios from "axios";
+import { useEffect, useState } from "react";
+function Accounts() {
+    const [accounts, setaccounts] = useState([]);
+    
+    //use effect is required for calling api not in the components
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/data")
+        .then((response) => {
+            setaccounts((previousState) => { //recoommend to use arrow function beause of updating in state
+                return response.data;
+            })
+        })
+        .catch((err) => {
+            console.log(`Error`, err)
+        })
+    },[])
+   
+    return <>
     </>
 }
 export default Accounts;
